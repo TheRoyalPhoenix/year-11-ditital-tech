422b820415be575aa883782327eece64c635b907
Daniel Lefebvre-Wigzell <danlef40@kingshigh.school.nz>
2022-03-04 10:54:24 +1300

    update 4 march

---
+++ test.py
@@ -0,0 +1,33 @@
+#first program
+"""
+print ("hello world")
+print (20+19)
+print (60+9)
+print(50/12)
+print (23*9)
+"""
+#secon program
+print ("daniel lefebvre wigzell")
+print ("ms")
+print ("king's high school")
+print ("15")
+print ("200 hours")
+print ("the year will be 2047")
+print ("115 years old ")
+print (658/7.9)
+print ("i have man favorit games ")
+print ("my favorit class is no longer this one")
+print ("34+65")
+print ("top text\nbottom text")
+print(5+10.0) #add a float to an integer 
+print(10-5.0) #subtract a float from an integer
+print (5/2)
+#print("hello world" + 24) 
+#print("hello world" - 24) 
+#print("hello world" / 5)
+#str(55+65)The first line above will actually evaluate the mathematical equation and then convert it to a string of  character. So it will convert it to a string containing the value “150”. 
+#int(55.0) The second line will convert the float to an integer – 55.
+#float(44+10)The third line will convert the equation into a float, and become 54.0. 
+print ("Legends never die when the world is calling you \n can you hear them screaming out your name? \n legends neve die they become a part of you \n every time you bleed reaching greatness \n legends never die")
+print ("if you do not give up,you still have a chance, Will Smith")
+print ("a string is a stament made by code ")
